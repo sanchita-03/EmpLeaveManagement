225a148855ec9c4914b4af6a51f67818d03cf5a0
Sanchita Mandhare <mandharesanchita@gmail.com>
2026-07-16 21:39:04 +0530

    undo changes

--- EMPSystem/EMPSystem.API/WeatherForecast.cs
+++ EMPSystem/EMPSystem.API/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace EMPSystem.API
 {
     public class WeatherForecast
     {
-        public DateOnly Dates { get; set; }
+        public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
 
